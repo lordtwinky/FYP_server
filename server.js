947d02a2d50d58cdaea786e3b4c0c91586afad90
lordtwinky <alex_cliffe2@outlook.com>
2018-04-30 10:33:39 +0100

    fixing alloworigin

--- server.js
+++ server.js
@@ -981,11 +981,11 @@ http.createServer((request, response) => {
         response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
         // Request headers you wish to allow
-        response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        response.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
 
         // Set to true if you need the website to include cookies in the requests sent
         // to the API (e.g. in case you use sessions)
-        response.setHeader('Access-Control-Allow-Credentials', true);
+        // response.setHeader('Access-Control-Allow-Credentials', true);
 
 
         var arrayQAs = JSON.stringify(sentenceArray(body));
