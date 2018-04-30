af70fe227cc7e0b901521a0fb498962294e7ca75
lordtwinky <alex_cliffe2@outlook.com>
2018-04-30 10:37:52 +0100

    another fix

--- server.js
+++ server.js
@@ -981,11 +981,12 @@ http.createServer((request, response) => {
         response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
         // Request headers you wish to allow
-        response.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+        // response.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+        response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
         // Set to true if you need the website to include cookies in the requests sent
         // to the API (e.g. in case you use sessions)
-        // response.setHeader('Access-Control-Allow-Credentials', true);
+        response.setHeader('Access-Control-Allow-Credentials', true);
 
 
         var arrayQAs = JSON.stringify(sentenceArray(body));
@@ -995,4 +996,4 @@ http.createServer((request, response) => {
         response.end();
 
     });
-}).listen(8000);
+}).listen(port);
